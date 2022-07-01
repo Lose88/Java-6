54b4bafb5d11d196f0baf08a0204e78a9a371e33
Lose88 <hotsting1888@gmail.com>
2022-07-01 13:30:17 +0300

    AddMainServiceAndTest

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,2 +1,13 @@
-package ru.netology.sqr;public class SQRService {
+package ru.netology.sqr;
+
+public class SQRService {
+    public int CalculateCountSqr(int start, int finish) {
+        int count = 0;
+        for (int i = 1; i <= finish; i++) {
+            if (i * i >= start && i * i <= finish) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
