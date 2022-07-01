aacf82c71ba512deed2ce00c6f6a3af8bb8f8e1c
Lose88 <hotsting1888@gmail.com>
2022-07-01 13:32:37 +0300

    ReformatCodeTest

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -5,7 +5,7 @@ import ru.netology.sqr.SQRService;
 
 public class SQRServiceTest {
     @Test
-    public void testExpectedActual(){
+    public void testExpectedActual() {
         SQRService service = new SQRService();
         int start = 10;
         int finish = 99;
@@ -14,8 +14,9 @@ public class SQRServiceTest {
         int actual = service.CalculateCountSqr(start, finish);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void testFinish100(){
+    public void testFinish100() {
         SQRService service = new SQRService();
         int start = 10;
         int finish = 100;
@@ -24,8 +25,9 @@ public class SQRServiceTest {
         int actual = service.CalculateCountSqr(start, finish);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void testStart0(){
+    public void testStart0() {
         SQRService service = new SQRService();
         int start = 0;
         int finish = 99;
